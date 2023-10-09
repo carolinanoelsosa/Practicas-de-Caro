b73c71762f46fbbbe7d79e0e3acd3496e1353dec
carolinanoelsosa <carolinanoelsosa@gmail.com>
2023-10-09 16:52:08 -0300

    Css styles for links

--- 2023_10_09/scriptTest.js
+++ 2023_10_09/scriptTest.js
@@ -1,5 +1,4 @@
 function displaySection(sectionId){
-    console.log()
     document.querySelectorAll('.section').forEach(s=> s.style.display = 'none')
     document.getElementById(sectionId).style.display = 'block'
 
